7b89037a1f3198a2113c26a7567524da92179e0b
aquaman122 <swj981128@gmail.com>
2026-01-12 14:41:06 +0900

    feat: 오류 수정 및 최적화 코드 추가

--- src/shared/lib/supabase/client.ts
+++ src/shared/lib/supabase/client.ts
@@ -17,3 +17,5 @@ export const supabaseClient = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
+
+export const createSupabaseBrowserClient = () => supabaseClient;
