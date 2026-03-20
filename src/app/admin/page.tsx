c15d0656e929afd9afb841acea9996570e1d8ef5
aquaman122 <swj981128@gmail.com>
2026-03-20 15:34:42 +0900

    feat: 프레이머에서 크로스 도메인 로그인 기능 추가

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
 
 import { VisitorChart, RevenueChart } from '@/src/features/admin/components/DashboardCharts';
 import StatCard from '@/src/features/admin/components/StatCard';
